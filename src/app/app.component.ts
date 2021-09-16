a5b1cf88be53d5bc9de056be715e36a73db080b0
lucimarsb <lucimarsan@outlook.com>
2021-09-15 21:06:44 -0300

    Trabalhando com *ngIf e Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { TransferenciaService } from './services/transferencia.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,12 +9,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'alura-angular-byteBank';
 
-  transferencias: any[] = [];
+  constructor(private service: TransferenciaService){}
 
   transferir($event){
-    console.log($event)
-    const transferencia = {...$event, data: new Date()};
-    this.transferencias.push(transferencia);
+    this.service.adicionar($event);
 
   }
 }
