9f3b79edec206dbf2237c5357b36bec18bc509d4
lucimarsb <lucimarsan@outlook.com>
2021-09-15 23:43:33 -0300

    Módulo RouterModule/add rotas aos components/RouterLink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AppRoutingModule } from './app-routing.module';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
@@ -8,12 +9,13 @@ import { ExtratoComponent } from './extrato/extrato.component';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [AppComponent, NovaTransferenciaComponent, ExtratoComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
   providers: [
     [
       { provide: LOCALE_ID, useValue: 'pt' },
